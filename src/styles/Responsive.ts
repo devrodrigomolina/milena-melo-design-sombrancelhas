45a9c4ce0b48ccedbfc5b39e24269cf5401e2dc2
Rodrigo Molina <75998678+devrodrigomolina@users.noreply.github.com>
2023-02-11 23:24:32 -0300

    feat: Criado arquivo de responsividades

---
+++ src/styles/Responsive.ts
@@ -0,0 +1,23 @@
+export const sizes = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '760px',
+  tabletM: '992px',
+  laptop: '1024px',
+  laptopM: '1366px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const devices = {
+  mobileS: `(max-width: ${sizes.mobileS})`,
+  mobileM: `(max-width: ${sizes.mobileM})`,
+  mobileL: `(max-width: ${sizes.mobileL})`,
+  tablet: `(max-width: ${sizes.tablet})`,
+  tabletM: `(max-width: ${sizes.tabletM})`,
+  laptop: `(max-width: ${sizes.laptop})`,
+  laptopM: `(max-width: ${sizes.laptopM})`,
+  laptopL: `(max-width: ${sizes.laptopL})`,
+  desktop: `(max-width: ${sizes.desktop})`,
+};
