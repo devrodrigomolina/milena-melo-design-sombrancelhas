d89d16698975b72d3affabda2804c7c5bf08fb92
Rodrigo Molina <75998678+devrodrigomolina@users.noreply.github.com>
2023-02-11 23:22:31 -0300

    feat: importado componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter } from "react-router-dom"
+import About from "./components/About/About"
 import Header from "./components/Header/Header"
 import TopHeader from "./components/Header/TopHeader/TopHeader"
 import Home from "./components/Home/Home"
@@ -5,12 +7,15 @@ import { GlobalStyles } from "./styles/Global"
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className="app">
       <Header />
       <TopHeader />
       <Home />
+      <About />
       <GlobalStyles />
     </div>
+    </BrowserRouter>
   )
 }
 
