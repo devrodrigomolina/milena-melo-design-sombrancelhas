78115962263513a11b886a775699da338c873d06
Rodrigo Molina <75998678+devrodrigomolina@users.noreply.github.com>
2023-02-12 23:12:13 -0300

    refactor: Adicionado imagem e prop position

--- src/components/Titles/Titles.tsx
+++ src/components/Titles/Titles.tsx
@@ -1,16 +1,17 @@
 import * as C from './styles'
-
+import divider from '../../assets/divider.png'
 interface TitlesTypes {
   title: string,
-  subtitle: string
+  subtitle: string,
+  position?: string | undefined
 }
 
-
-const Titles = ({title, subtitle}: TitlesTypes) => {
+const Titles = ({title, subtitle, position}: TitlesTypes) => {
   return (
-    <C.ContainerTitles>
+    <C.ContainerTitles position={position}>
         <p className='quality'>{subtitle}</p>
         <h2 className='welcome'>{title}</h2>
+        <img src={divider} />
     </C.ContainerTitles>
   )
 }
