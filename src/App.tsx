07b7e612f8c012707c37ce532a6ebf469c0f5e34
Rodrigo Molina <75998678+devrodrigomolina@users.noreply.github.com>
2023-02-12 23:05:40 -0300

    feat: Importado os componentes criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter } from "react-router-dom"
 import About from "./components/About/About"
+import Footer from "./components/Footer/Footer"
+import Gallery from "./components/Gallery/Gallery"
 import Header from "./components/Header/Header"
 import TopHeader from "./components/Header/TopHeader/TopHeader"
 import Home from "./components/Home/Home"
+import Testimonials from "./components/Testimonials/Testimonials"
 import { GlobalStyles } from "./styles/Global"
 
 const App = () => {
@@ -13,6 +16,9 @@ const App = () => {
       <TopHeader />
       <Home />
       <About />
+      <Gallery />
+      <Testimonials />
+      <Footer />
       <GlobalStyles />
     </div>
     </BrowserRouter>
